66e61ca3a91f13b1399ff177910070018cfa91a1
Techakkord <jonakkord@mail.ru>
2020-07-18 01:06:48 +0600

    Добавьте файлы проекта.

---
+++ C5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace C5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = args[0];
+            string target = args[1];
+            if (!(File.Exists(source)))
+            {
+                Console.Write("Нет исходного файла");
+                Environment.Exit(0);
+            }
+            if (File.Exists(target))
+            {
+                Console.Write("Целевой файл уже существует");
+                Environment.Exit(0);
+            }
+            if (File.Exists("tmp.sql"))
+            {
+                File.Delete("tmp.sql");
+                Console.Write("Контроль зачистки");
+            }
+            //вызов сторонней программы, которая создаёт dump базы данных SQL Compact в формате *.sql
+            var prc = new ProcessStartInfo("ExportSQLCE40.exe");
+            prc.ArgumentList.Add("Data Source =" + source + ";");
+            prc.ArgumentList.Add("tmp.sql");
+            prc.ArgumentList.Add("sqlite");
+            var i = Process.Start(prc);
+            i.WaitForExit();
+            // вызов сторонней программы, которая создаёт из dump'а  формирует базу данных sqlite
+            var prc2 = new ProcessStartInfo("sqlite3.exe");
+            prc2.ArgumentList.Add(target);
+            prc2.ArgumentList.Add(".read tmp.sql");
+            var i2 = Process.Start(prc2);
+            i2.WaitForExit();
+
+        }
+
+    }
+
+}
