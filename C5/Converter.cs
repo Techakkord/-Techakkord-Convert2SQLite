92c560cd1b26c40bf471dbfc9d84181684b9f3c8
Techakkord <jonakkord@mail.ru>
2020-07-18 17:39:36 +0600

    ...

--- C5/Converter.cs
+++ C5/Converter.cs
@@ -12,6 +12,7 @@ namespace C5
             //Пример запуска конвертации в консоли: C:\Users\jonak\source\repos\C5\C5\bin\Debug\netcoreapp3.1> .\C5.exe IsueDic.sdf 1.sqlite
             string source = args[0];
             string target = args[1];
+            //Отлавливаем некоторые возможные ошибки 
             if (!(File.Exists(source)))
             {
                 Console.Write("Нет исходного файла");
@@ -27,6 +28,31 @@ namespace C5
                 File.Delete("tmp.sql");
                 Console.Write("Контроль зачистки");
             }
+            if (!(File.Exists("ExportSqlCE40.exe")))
+            {
+                Console.Write("В директории нет необходимых рабочих файлов");
+                Environment.Exit(0);
+            }
+            if (!(File.Exists("QuickGraph.Data.dll")))
+            {
+                Console.Write("В директории нет необходимых рабочих файлов");
+                Environment.Exit(0);
+            }
+            if (!(File.Exists("QuickGraph.dll")))
+            {
+                Console.Write("В директории нет необходимых рабочих файлов");
+                Environment.Exit(0);
+            }
+            if (!(File.Exists("QuickGraph.Graphviz.dll")))
+            {
+                Console.Write("В директории нет необходимых рабочих файлов");
+                Environment.Exit(0);
+            }
+            if (!(File.Exists("sqlite3.exe")))
+            {
+                Console.Write("В директории нет необходимых рабочих файлов");
+                Environment.Exit(0);
+            }
             //вызов сторонней программы, которая создаёт dump базы данных SQL Compact в формате *.sql
             var prc = new ProcessStartInfo("ExportSQLCE40.exe");
             prc.ArgumentList.Add("Data Source =" + source + ";");
@@ -34,7 +60,7 @@ namespace C5
             prc.ArgumentList.Add("sqlite");
             var i = Process.Start(prc);
             i.WaitForExit();
-            // вызов сторонней программы, которая создаёт из dump'а  формирует базу данных sqlite
+            // вызов сторонней программы, которая из dump'а  формирует базу данных sqlite
             var prc2 = new ProcessStartInfo("sqlite3.exe");
             prc2.ArgumentList.Add(target);
             prc2.ArgumentList.Add(".read tmp.sql");
